965e48bd725758e34f4b2b9714fd05e0c289a178
goonle <junhuh741@gmail.com>
2026-02-24 16:08:07 +1300

    Fix build prerender error

--- order-history/app/dashboard/page.tsx
+++ order-history/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import DashboardShell from "../components/dashboard/DashboardShell";
 import { listVendorsAction } from "@/server/actions/vendor.action";
 import { Vendor } from "@/app/model/vendor";
+import { redirect } from "next/navigation";
+
+// 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function DashboardPage() {
   const res = await listVendorsAction();
+
   if (!res.ok) {
+    // if login failed, redirect to login page
+    if (res.code === "UNAUTHORIZED") {
+      redirect("/login");
+    }
     throw new Error("Failed to load vendors");
   }
 
